ffa6b93d61d0d80e5d56d474a13b5a0d277ea4ee
sgoldman <sgoldman216@gmail.com>
2015-04-11 18:56:02 -0600

    Removed unneccessary parent folder

--- NodeJS Virtual Hand/config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-	'url' : 'mongodb://sgoldman:sgoldman@ds059471.mongolab.com:59471/app_users' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-};
